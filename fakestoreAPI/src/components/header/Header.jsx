0d5d7fa11308a46a4d9228bee9ba1506516ba228
thimappathesecond <hariharakumar08@gmail.com>
2024-09-18 16:06:45 +0530

    Guvi-TaskFive

---
+++ fakestoreAPI/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Header.css";
+const Header = () => {
+  return (
+    <header className="header__logo">
+      <h1>FakeStore.com</h1>
+    </header>
+  );
+};
+
+export default Header;
